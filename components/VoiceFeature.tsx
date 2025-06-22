d7653efa102d8ba2e39b03f56492ee2cc3f93aaf
Yves Hughes <yves@nextdoormultimedia.com>
2025-06-21 22:43:01 -0700

    fixed deployment errors

--- components/VoiceFeature.tsx
+++ components/VoiceFeature.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
-import TypewriterText from './TypewriterText';
 import { useLiveMode } from '@/contexts/LiveModeContext';
 
 export default function VoiceFeature() {
